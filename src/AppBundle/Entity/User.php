c39ecaa98faa318723a6c16dbbb0aa13a90ba581
mariellelau <marielle.lautrou@gmail.com>
2017-10-18 13:53:10 +0200

    surcharge fosuser

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -4,6 +4,7 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -18,8 +19,48 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * Overridden so that username is now optional
+     *
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->setUsername($email);
+        return parent::setEmail($email);
+    }
+
     public function __construct()
     {
         parent::__construct();
+    }
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $firstName;
+
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $city;
+
+    public function getCity()
+    {
+        return $this->city;
+    }
+    public function setCity($city)
+    {
+        $this->city = $city;
     }
 }
---
+++ src/AppBundle/Form/RegistrationType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Form;
+
+use FOS\UserBundle\Form\Type\RegistrationFormType;
+use FOS\UserBundle\Util\LegacyFormHelper;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('plainPassword', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), array(
+                'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
+                'options' => array('translation_domain' => 'FOSUserBundle'),
+                'first_options' => array('label' => 'Mot de passe'),
+                'second_options' => array('label' => 'Confirmation du mot de passe'),
+                'invalid_m  essage' => 'Les 2 mots de passe saisis ne correspondent pas',
+            ))
+            ->add('city')
+            ->remove('username')
+        ;
+    }
+
+    public function getParent()
+    {
+        return RegistrationFormType::class;
+    }
+
+    public function getName()
+    {
+        return RegistrationType::class;
+    }
+}
